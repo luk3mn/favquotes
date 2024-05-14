0f1168ac477f6c83f47562795ae38edbb013a37b
luk3mn <lucasrenan2015@gmail.com>
2024-05-14 17:41:40 -0300

    config: project initializing

---
+++ src/main/java/com/luke/favquotes/FavquotesApplication.java
@@ -0,0 +1,13 @@
+package com.luke.favquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavquotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavquotesApplication.class, args);
+	}
+
+}
